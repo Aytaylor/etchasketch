3b09b915c40e48b936c01a94feb1b7011339ab57
aytaylor <aytaylor33@gmail.com>
2020-10-07 14:52:35 -0400

    First commit of project

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const container = document.getElementById("container");
+const buttonReset = document.getElementById("reset");
+
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    
+    document.querySelectorAll('.grid-item').forEach(item => item.remove());
+
+    for (let c = 0; c < (rows * cols); c++) {
+        const cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+
+    const blocks = Array.from(document.querySelectorAll(".grid-item"));
+    blocks.forEach(cell => cell.addEventListener("mouseenter", event => {
+        fill(cell);
+    }));
+};
+
+
+function resetPrompt() {
+    let resetInput = parseInt(prompt("Enter how many rows to generate. (Between 1-100)"));
+    return resetInput;
+};
+
+function reset() {    
+    let newRows = resetPrompt();
+    while (newRows < 1 || newRows > 100 || new) {
+        newRows = resetPrompt();
+    }
+    console.log(newRows);
+    makeRows(newRows, newRows);
+}
+
+function fill(cell) {
+    cell.classList.add('filled');
+}
+
+buttonReset.addEventListener("click", event => {
+    reset();
+});
+
+// build first grid
+makeRows(100, 100);
